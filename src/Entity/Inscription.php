f63bfffa8fabf398837c8ecae281c89cd98a0b5d
Alexoos <95075956+Alexoos@users.noreply.github.com>
2023-03-02 12:36:40 +0100

    🍱 Ajout Entity
    Mise en place des Entitys

---
+++ src/Entity/Inscription.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InscriptionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InscriptionRepository::class)]
+class Inscription
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateInscription = null;
+
+    #[ORM\OneToOne()]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Compte $compte = null;
+
+    #[ORM\OneToMany(mappedBy: 'inscription', targetEntity: Restauration::class, orphanRemoval: true)]
+    private Collection $restaurations;
+
+    #[ORM\OneToMany(mappedBy: 'inscription', targetEntity: Nuite::class, orphanRemoval: true)]
+    private Collection $nuites;
+
+    #[ORM\ManyToMany(targetEntity: Atelier::class, inversedBy: 'inscriptions')]
+    private Collection $ateliers;
+
+    public function __construct()
+    {
+        $this->restaurations = new ArrayCollection();
+        $this->nuites = new ArrayCollection();
+        $this->ateliers = new ArrayCollection();
+    }
+
+    
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateInscription(): ?\DateTimeInterface
+    {
+        return $this->dateInscription;
+    }
+
+    public function setDateInscription(\DateTimeInterface $dateInscription): self
+    {
+        $this->dateInscription = $dateInscription;
+
+        return $this;
+    }
+
+    public function getCompte(): ?Compte
+    {
+        return $this->compte;
+    }
+
+    public function setCompte(Compte $compte): self
+    {
+        $this->compte = $compte;
+
+        return $this;
+    }
+
+    public function getRestaurations(): ?Restauration
+    {
+        return $this->restaurations;
+    }
+
+    public function setRestaurations(?Restauration $restaurations): self
+    {
+        $this->restaurations = $restaurations;
+
+        return $this;
+    }
+
+    public function addRestauration(Restauration $restauration): self
+    {
+        if (!$this->restaurations->contains($restauration)) {
+            $this->restaurations->add($restauration);
+            $restauration->setInscription($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRestauration(Restauration $restauration): self
+    {
+        if ($this->restaurations->removeElement($restauration)) {
+            // set the owning side to null (unless already changed)
+            if ($restauration->getInscription() === $this) {
+                $restauration->setInscription(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Nuite>
+     */
+    public function getNuites(): Collection
+    {
+        return $this->nuites;
+    }
+
+    public function addNuite(Nuite $nuite): self
+    {
+        if (!$this->nuites->contains($nuite)) {
+            $this->nuites->add($nuite);
+            $nuite->setInscription($this);
+        }
+
+        return $this;
+    }
+
+    public function removeNuite(Nuite $nuite): self
+    {
+        if ($this->nuites->removeElement($nuite)) {
+            // set the owning side to null (unless already changed)
+            if ($nuite->getInscription() === $this) {
+                $nuite->setInscription(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Atelier>
+     */
+    public function getAteliers(): Collection
+    {
+        return $this->ateliers;
+    }
+
+    public function addAtelier(Atelier $atelier): self
+    {
+        if (!$this->ateliers->contains($atelier)) {
+            $this->ateliers->add($atelier);
+        }
+
+        return $this;
+    }
+
+    public function removeAtelier(Atelier $atelier): self
+    {
+        $this->ateliers->removeElement($atelier);
+
+        return $this;
+    }
+}
---
+++ src/Entity/Restauration.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RestaurationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RestaurationRepository::class)]
+class Restauration
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateRestauration = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $typeRepas = null;
+
+    #[ORM\ManyToOne(inversedBy: 'restaurations')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Inscription $inscription = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateRestauration(): ?\DateTimeInterface
+    {
+        return $this->dateRestauration;
+    }
+
+    public function setDateRestauration(\DateTimeInterface $dateRestauration): self
+    {
+        $this->dateRestauration = $dateRestauration;
+
+        return $this;
+    }
+
+    public function getTypeRepas(): ?string
+    {
+        return $this->typeRepas;
+    }
+
+    public function setTypeRepas(string $typeRepas): self
+    {
+        $this->typeRepas = $typeRepas;
+
+        return $this;
+    }
+
+    public function getInscription(): ?Inscription
+    {
+        return $this->inscription;
+    }
+
+    public function setInscription(?Inscription $inscription): self
+    {
+        $this->inscription = $inscription;
+
+        return $this;
+    }
+}
