828716d80258fb5d1f68bc7c60d482ba402b5dfe
Alexoos <95075956+Alexoos@users.noreply.github.com>
2023-03-07 16:37:28 +0100

    scss login form

--- assets/app.js
+++ assets/app.js
@@ -9,6 +9,7 @@ import { registerReactControllerComponents } from '@symfony/ux-react';
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
+
 // start the Stimulus application
 import './bootstrap';
 registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
