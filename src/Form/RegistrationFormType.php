cd246529cf6dcac0c3f2fe2b776619429a5de739
Alexoos <95075956+Alexoos@users.noreply.github.com>
2023-03-31 13:09:40 +0200

    🚧 suite message erreur

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -13,50 +13,54 @@ use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Validator\Constraints\IsFalse;
-class RegistrationFormType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+use Symfony\Component\Validator\Constraints\Count;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class RegistrationFormType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
         $builder
-                ->add('numLicence', NumberType::class,[
-                'attr' => [
-                    'min'=>00000000001,
-                    'max'=>99999999999,
-                ],
-                    
-                'constraints' => [
-                    new isFalse([
-                        'message' => 'Veuillez renseigner un numéro de licencié valide !',
-                    ]),
-                    
-                ]
-                    
-  
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Veuillez renseigner un mot de passe',
-                    ]),
-                    new Length([
-                        'min' => 8,
-                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 20,
-                    ]),
-                ],
-            ])
+                ->add('numLicence', NumberType::class, [
+                    'attr' => [
+                        'min' => 00000000001,
+                        'max' => 99999999999,
+                    ],
+                    'constraints' => [
+                        new Length([
+                            'min' => 11,
+                            'exactMessage' => 'Le numéro de licencié doit contenir 11 caractères !',
+                            'max' => 11,
+                        ]),
+                         new Regex([
+                            'message' => 'Veuillez saisir un numéro de licencié valide !',
+                            'pattern' => '/^[0-9]*$/',
+                        ]),
+                    ]
+                ])
+                ->add('plainPassword', PasswordType::class, [
+                    // instead of being set onto the object directly,
+                    // this is read and encoded in the controller
+                    'mapped' => false,
+                    'attr' => ['autocomplete' => 'new-password'],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez renseigner un mot de passe',
+                                ]),
+                        new Length([
+                            'min' => 8,
+                            'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 20,
+                                ]),
+                    ],
+                ])
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
+    public function configureOptions(OptionsResolver $resolver): void {
         $resolver->setDefaults([
             'data_class' => Compte::class,
         ]);
     }
+
 }
